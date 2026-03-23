848d223f864e4a102986247fae82b198e8bfc1f5
lewhfree <48800753+lewhfree@users.noreply.github.com>
2026-03-22 23:52:44 -0600

    initial commit

---
+++ main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+
+	"github.com/gin-gonic/gin"
+	glide "github.com/valkey-io/valkey-glide/go/v2"
+	"github.com/valkey-io/valkey-glide/go/v2/config"
+	"github.com/valkey-io/valkey-glide/go/v2/pipeline"
+)
+
+type jsonMultiRequest struct {
+	HashList []string `json:"hashlist"`
+}
+type jsonSingleRequest struct {
+	Hash string `json:"hash"`
+}
+
+func handleSingleHashLookup(client *glide.Client, c *gin.Context) {
+	jsonData, err := io.ReadAll(c.Request.Body)
+	if err != nil {
+		fmt.Println("error reading request body: ", err)
+		return
+	}
+	var hashData jsonSingleRequest
+	err = json.Unmarshal(jsonData, &hashData)
+	if err != nil {
+		fmt.Println("error unmarshalling json: ", err)
+		return
+	}
+
+	fmt.Println(hashData.Hash)
+
+	keys, err := client.LRange(context.Background(), hashData.Hash, 0, -1)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+	fmt.Println(keys)
+}
+
+func handleMultiGetLookup(client *glide.Client, c *gin.Context) {
+
+	jsonData, err := io.ReadAll(c.Request.Body)
+	if err != nil {
+		fmt.Println("error reading request body: ", err)
+		return
+	}
+	var hashData jsonMultiRequest
+	err = json.Unmarshal(jsonData, &hashData)
+	if err != nil {
+		fmt.Println("error unmarshalling json: ", err)
+		return
+	}
+
+	batch := pipeline.NewStandaloneBatch(false)
+
+	for _, element := range hashData.HashList {
+		batch.LRange(element, 0, -1)
+	}
+
+	data, err := client.Exec(context.Background(), *batch, false)
+
+	if err != nil {
+		fmt.Println("error on batch exec: ", err)
+		return
+	}
+
+	fmt.Println("data from batch: ", data)
+	// fmt.Println(hashData.HashList)
+	// keys, err := client.MGet(context.Background(), hashData.HashList)
+	// if err != nil {
+	// 	fmt.Println("Error: ", err)
+	// 	return
+	// }
+	// fmt.Println(keys)
+}
+
+func main() {
+	host := "localhost"
+	port := 6379
+
+	config := config.NewClientConfiguration().
+		WithAddress(&config.NodeAddress{Host: host, Port: port})
+	client, err := glide.NewClient(config)
+	if err != nil {
+		fmt.Println("There was an error: ", err)
+		return
+	}
+
+	router := gin.Default()
+	router.POST("/singleget", func(c *gin.Context) { handleSingleHashLookup(client, c) })
+	router.POST("/multiget", func(c *gin.Context) { handleMultiGetLookup(client, c) })
+	router.Run()
+
+	client.Close()
+}
