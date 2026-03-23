46ac2e2b2cedd379e5b71c18fbe37859a92ba2b3
lewhfree <48800753+lewhfree@users.noreply.github.com>
2026-03-23 13:13:03 -0600

    working on binning

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/binary"
 	"encoding/json"
 	"fmt"
 	"github.com/gin-gonic/gin"
@@ -12,14 +13,9 @@ import (
 )
 
 type jsonMultiRequest struct {
-	HashList []string `json:"hashlist"`
+	HashList   []string `json:"hashlist"`
+	OffsetList []uint32 `json:"offsetlist"`
 }
-type jsonSingleRequest struct {
-	Hash string `json:"hash"`
-}
-
-type songId uint32
-type offset uint32
 
 func packTo64(songId uint32, offset uint32) uint64 {
 	return (uint64(songId) << 32) | uint64(offset)
@@ -31,27 +27,70 @@ func unpackFrom64(packed uint64) (uint32, uint32) {
 	return songId, offset
 }
 
-func handleSingleHashLookup(client *glide.Client, c *gin.Context) {
-	jsonData, err := io.ReadAll(c.Request.Body)
-	if err != nil {
-		fmt.Println("error reading request body: ", err)
-		return
+func arrayAnyToNestedString(array []any) [][]string {
+	var tmp [][]string
+	for _, element := range array {
+		stringsSlice, ok := element.([]string)
+		if !ok {
+			fmt.Println("error on casting to array string")
+			return nil
+		}
+		tmp = append(tmp, stringsSlice)
 	}
-	var hashData jsonSingleRequest
-	err = json.Unmarshal(jsonData, &hashData)
-	if err != nil {
-		fmt.Println("error unmarshalling json: ", err)
-		return
+	return tmp
+}
+
+func nestedStrToNestedInt(array [][]string) [][]uint64 {
+	var tmp [][]uint64
+	for i, _ := range array {
+		tmp = append(tmp, []uint64{})
+		for _, element2 := range array[i] {
+			packed := binary.LittleEndian.Uint64([]byte(element2))
+			tmp[i] = append(tmp[i], packed)
+			songId, offset := unpackFrom64(packed)
+			fmt.Printf("SongID=%d, Offset=%d\n", songId, offset)
+		}
 	}
 
-	fmt.Println(hashData.Hash)
+	return tmp
+}
 
-	keys, err := client.LRange(context.Background(), hashData.Hash, 0, -1)
-	if err != nil {
-		fmt.Println("Error: ", err)
-		return
+// func extractOffsets(nestedInts [][]uint64) ([][]uint32, [][]uint32) {
+// 	var ids [][]uint32
+// 	var off [][]uint32
+// 	for i, element := range nestedInts {
+// 		ids = append(ids, []uint32{})
+// 		off = append(off, []uint32{})
+// 		for _, element2 = range element {
+// 			tmpid, tmpoff = unpackFrom64(element2)
+// 			ids[i][j], off[i][j] = unpackFrom64(nestedInts[i][j])
+// 		}
+// 	}
+// }
+
+func extractOffsetPairs(nestedInts [][]uint64) ([][]uint32, [][]uint32) {
+	var ids [][]uint32
+	var off [][]uint32
+	for i, element := range nestedInts {
+		ids = append(ids, []uint32{})
+		off = append(off, []uint32{})
+		for _, element2 := range element {
+			tmpid, tmpoffset := unpackFrom64(element2)
+			ids[i] = append(ids[i], tmpid)
+			off[i] = append(off[i], tmpoffset)
+		}
 	}
-	fmt.Println(keys)
+	return ids, off
+}
+
+func determinePeak(nestedInts [][]uint64, clientOffsets []uint32) int {
+	// var counterMap = make(map[uint32]map[uint32]int)
+	//    songid     offset  count
+	songIds, offsets := extractOffsetPairs(nestedInts)
+	if (len(songIds) == len(offsets)) && (len(clientOffsets) == len(offsets)) {
+		print("all the same length just checking. ")
+	}
+	return 0
 }
 
 func handleMultiGetLookup(client *glide.Client, c *gin.Context) {
@@ -79,8 +118,11 @@ func handleMultiGetLookup(client *glide.Client, c *gin.Context) {
 		fmt.Println("error on batch exec: ", err)
 		return
 	}
-
-	fmt.Println("data from batch: ", data)
+	nestedStrData := arrayAnyToNestedString(data)
+	var nestedIntData [][]uint64 = nestedStrToNestedInt(nestedStrData)
+	determinePeak(nestedIntData, hashData.OffsetList)
+	fmt.Println(hashData.OffsetList)
+	fmt.Println(nestedIntData)
 }
 
 func main() {
@@ -96,7 +138,6 @@ func main() {
 	}
 
 	router := gin.Default()
-	router.POST("/singleget", func(c *gin.Context) { handleSingleHashLookup(client, c) })
 	router.POST("/multiget", func(c *gin.Context) { handleMultiGetLookup(client, c) })
 	router.Run()
 
