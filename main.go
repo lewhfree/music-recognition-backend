d74d99a5fc981d657fda2f3a270d3fdaa3d88fd3
lewhfree <48800753+lewhfree@users.noreply.github.com>
2026-03-26 09:41:43 -0600

    Prep for duplicate search

--- main.go
+++ main.go
@@ -197,6 +197,8 @@ func ingest(client *glide.Client, c *gin.Context) {
 
 	metaKey := fmt.Sprintf("meta:%d", dbNewSongId)
 	batch.Set(metaKey, data.Spotify)
+	metaKey2 := fmt.Sprintf("spotify:%s", data.Spotify)
+	batch.Set(metaKey2, string(dbNewSongId))
 	_, err = client.Exec(c.Request.Context(), *batch, false)
 	if err != nil {
 		fmt.Println("Error ingesting batch. valkey: ", err)
